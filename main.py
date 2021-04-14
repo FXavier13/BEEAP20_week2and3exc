30b61b0036e2fe7a614b71c15da0d6bbe1134265
FXavier13 <francis.manyonje@student.hamk.fi>
2021-04-14 06:56:25 +0300

    List Box display of "List of Cities" added

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ class App:
         self.__Load_CSV["command"] = self.__Load_CSV_command
 
         self.__Cities_List= ttk.Combobox(root)
+        self.__Cities_List["text"] = "List of Cities"
         self.__Cities_List.place(x=350, y=50, width=80, height=25)
         self.__Cities_List.bind("<<ComboboxSelected>>", self.__comboBoxCb)
 
