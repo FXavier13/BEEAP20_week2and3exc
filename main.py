d17e89bcf5962c46f7ad3719fc8e06e08a1ef6cc
FXavier13 <francis.manyonje@student.hamk.fi>
2021-04-15 07:39:50 +0300

    Indentation eror 4 fixed

--- main.py
+++ main.py
@@ -61,11 +61,10 @@ class App:
 
         for i in range(1):
             for j in range(2):
-            frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
+                frame = tk.Frame(
+                    master=window,
+                        relief=tk.RAISED,
+                             borderwidth=1)
         frame.grid(row=i, column=j, padx=15, pady=15)
         CitySelect_Label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
         FileName_Label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
