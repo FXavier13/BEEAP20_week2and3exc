e8997df8afb34ffb9911aa80f52be094a4f6ca6e
FXavier13 <francis.manyonje@student.hamk.fi>
2021-04-18 17:10:22 +0300

    .

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ class Plot:
 plt.show() 
 
 self.__Monthly_KWH_Average = FigureCanvasTkAgg(OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv'))
-self.__Monthly_KWH_Average = tk.Canvas(root)
+self.__Monthly_KWH_Average = tk.Canvas(OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv'))
 self.__Monthly_KWH_Average.place(x=50, y=130, width=234, height=140)
 Monthly_KWH_Average.draw() #Or Canvas.draw()?
 
