18d21136c07cab87f380a4ada362203a6b665472
FXavier13 <francis.manyonje@student.hamk.fi>
2021-04-18 16:51:18 +0300

    Indentation error fixed

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def __Load_CSV_command(self):
             def __comboBoxCb(self, event=None):
                 self.__subdf = self.__df.loc[self.__df['COMMUNITY AREA NAME'] == self.__Cities_List.get()]
             print(self.__subdf.head())
-        fig1 = Figure(figsize=(self.__GLineEdit_392.winfo_width, self.__GLineEdit_392.winfo_height), dpi=100)
+            fig1 = Figure(figsize=(self.__GLineEdit_392.winfo_width, self.__GLineEdit_392.winfo_height), dpi=100)
         ax1 = fig1.add_subplot(111)
         self.__subdf.iloc[:, range(self.__subdf.columns.get_loc['KWH JANUARY 2010'], 12)].mean().plot.bar(ax=ax1)
 
