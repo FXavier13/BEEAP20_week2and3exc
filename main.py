45ba17ab4fe913438c8b3c0f09a02a142cc193cc
FXavier13 <francis.manyonje@student.hamk.fi>
2021-04-18 16:37:53 +0300

    Indentation error fixed

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ toolbar=NavigationToolbar2Tk(Monthly_KWH_Average,root)
 Monthly_KWH_Average.get_tk_widget().pack()
 
         #Button for displaying the plot
-        Energy_Usage_Summary_button=Button(self=root, command = plot, height =2, width = 10,text ="Plot Summary")
+Energy_Usage_Summary_button=Button(self=root, command = plot, height =2, width = 10,text ="Plot Summary")
 
         #Placing the button onto the main window
         Energy_Usage_Summary_button.pack()
