b330d7637f868b51abc6714c16ae18deee29a14d
FXavier13 <francis.manyonje@student.hamk.fi>
2021-04-21 13:32:25 +0300

    Re-worked file for Exercise 2 and 3

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 07:35:43 2021
+
+@author: Francis
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,186 +11,166 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 import tkinter.font as tkFont
+from pandas import DataFrame
 
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
-
 class App:
-    def __init__(self, root,OriginalDataSet):
-        self.root= OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv')
-        self.OriginalDataSet=OriginalDataSet.dropna()
-        OriginalDataSet.head()
-        print(OriginalDataSet)
-        # setting title
+    def __init__(self, root):
+        dpi = root.winfo_fpixels(100)
+         # Creating the frame for Buttons & Root controls
+        self.controls = tk.Frame(root)
+        self.controls.pack(ipadx=10, ipady=10)
+         # frame for  Bar charts
+        self.graphs = tk.Frame(root)
+        self.graphs.pack(side=tk.BOTTOM,
+                             padx=5, pady=5,
+                             fill=tk.BOTH, expand=True)
         root.title("Energy Usage 2010")
-        # setting window size
-        width = 650
-        height = 500
+        # Expanded Window Size to fit a full data display
+        width = 1200
+        height = 700
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height,
                                     (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
-
-        self.__Load_CSV = tk.Button(root)
-        self.__Load_CSV["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times', size=8)
-        self.__Load_CSV["font"] = ft
-        self.__Load_CSV["fg"] = "#000000"
-        self.__Load_CSV["justify"] = "center"
-        self.__Load_CSV["text"] = "Load CSV File"
-        self.__Load_CSV.place(x=70, y=50, width=70, height=35)
-        self.__Load_CSV["command"] = self.__Load_CSV_command
-
-        self.__Cities_List= ttk.Combobox(root)
-        self.__Cities_List["justify"] = "center" #Getting the List Box to display all characters of the City name
-        self.__Cities_List.place(x=400, y=50, width=120, height=25)
-        self.__Cities_List.bind("<<ComboboxSelected>>", self.__comboBoxCb)
-
-       
-        self.__FileName_Label = tk.Label(root)
+        #File Loading Button
+        self.loadbutton = tk.Button(root)
+        self.loadbutton["bg"] = "#efefef"
         ft = tkFont.Font(family='Times', size=10)
-        self.__FileName_Label["font"] = ft
-        self.__FileName_Label["fg"] = "#333333"
-        self.__FileName_Label["justify"] = "center"
-        self.__FileName_Label["text"] = "Energy Usage 2010"
-        self.__FileName_Label.place(x=150, y=50, width=110, height=25)
+        self.loadbutton["font"] = ft
+        self.loadbutton["fg"] = "#000000"
+        self.loadbutton["justify"] = "center"
+        self.loadbutton["text"] = "Load File"
+        self.loadbutton.place(x=70, y=15, width=70, height=25,)
+        self.loadbutton["command"] = self.loadfile_command
 
-        self.__CitySelect_Label = tk.Label(root)
+        self.selectorcombo = ttk.Combobox(root)
+        self.selectorcombo.place(x=550, y=15, width=80, height=25)
+        self.selectorcombo.bind("<<ComboboxSelected>>", self.hCombo_city_selected)
+        #Displaying the loaded File Name/Path through a Label
+        self.filename = tk.Label(root)
         ft = tkFont.Font(family='Times', size=10)
-        self.__CitySelect_Label["font"] = ft
-        self.__CitySelect_Label["fg"] = "#333333"
-        self.__CitySelect_Label["justify"] = "center"
-        self.__CitySelect_Label["text"] = "Select City"
-        self.__CitySelect_Label.place(x=300, y=50, width=110, height=25)
-        
-        #TRIAL CODE FOR CANVAS 1
+        self.filename["font"] = ft
+        self.filename["fg"] = "#333333"
+        self.filename["justify"] = "center"
+        self.filename["text"] = "File Name"
+        self.filename.place(x=150, y=15, width=200, height=25)
+        #Select City Label
+        self.townselectorlabel = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        self.townselectorlabel["font"] = ft
+        self.townselectorlabel["fg"] = "#333333"
+        self.townselectorlabel["justify"] = "center"
+        self.townselectorlabel["text"] = "Select City"
+        self.townselectorlabel.place(x=450, y=15, width=70, height=25)
+       #Canvases
+        self.Canvas_upleft = tk.Frame(self.graphs)
+        self.Canvas_upleft.place(relx=0, rely=0,
+                                   relwidth=0.5, relheight=0.5)
+        self.fig1 = plt.figure(dpi=100)
+        self.ax1 = self.fig1.add_subplot(111)
+        self.ax1.text(0.1,0.5,"Plot 1", fontsize=10)
+        self.chart1 = FigureCanvasTkAgg(self.fig1, self.Canvas_upleft)
+        self.chart1.get_tk_widget().pack(padx=5, pady=5,
+                                         side=tk.BOTTOM,
+                                         fill=tk.BOTH, expand=True)
+        self.Canvas_upright = tk.Frame(self.graphs)
+        self.Canvas_upright.place(relx=0.5, rely=0,
+                                    relwidth=0.5, relheight=0.5)
+        self.fig2 = plt.figure(dpi=100)
+        self.ax2 = self.fig2.add_subplot(111)
+        self.ax2.text(0.1,0.5,"Plot 2 ", fontsize=10)
+        self.chart2 = FigureCanvasTkAgg(self.fig2, self.Canvas_upright)
+        self.chart2.get_tk_widget().pack(padx=5, pady=5,
+                                         side=tk.BOTTOM,
+                                         fill=tk.BOTH, expand=True)
+        self.Canvas_botleft = tk.Frame(self.graphs)
+        self.Canvas_botleft.place(relx=0, rely=0.5,
+                                    relwidth=0.5, relheight=0.5)
+        self.fig3 = plt.figure(dpi=100)
+        self.ax3 = self.fig3.add_subplot(111)
+        self.ax3.text(0.1,0.5,"Plot 3 ", fontsize=10)
+        self.chart3 = FigureCanvasTkAgg(self.fig3, self.Canvas_botleft)
+        self.chart3.get_tk_widget().pack(padx=5, pady=5,
+                                         side=tk.BOTTOM,
+                                         fill=tk.BOTH, expand=True)
+        self.Canvas_botright = tk.Frame(self.graphs)
+        self.Canvas_botright.place(relx=0.5, rely=0.5,
+                                     relwidth=0.5, relheight=0.5)
+        self.fig4 = plt.figure(dpi=100)
+        self.ax4 = self.fig4.add_subplot(111)
+        self.ax4.text(0.1,0.5,"Plot 4", fontsize=10)
+        self.chart4 = FigureCanvasTkAgg(self.fig4, self.Canvas_botright)
+        self.chart4.get_tk_widget().pack(padx=5, pady=5,
+                                         side=tk.BOTTOM,
+                                         fill=tk.BOTH, expand=True)
+    def loadfile_command(self):
+        filePath = fd.askopenfilename(initialdir='.')
+        try:
+            self.csv_contents = pd.read_csv(filePath)
+            self.csv_cleaned = self.csv_contents.dropna()
+            self.selectorcombo['values'] = list(self.csv_cleaned['COMMUNITY AREA NAME'].unique())
+            self.filename["text"] = filePath
+        except:
+            tk.messagebox.showinfo("Nope","NOPE ")
 
-        #The Energy Usage 2010 Dataset that will be used to obtain the plots 
-        OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv')
-        OriginalDataSet=OriginalDataSet.dropna()
-        OriginalDataSet.head()
-        print(OriginalDataSet)
+    
+    def hCombo_city_selected(self, event=None):
+        selected_city = self.selectorcombo.get()
+        print(f"Selected city: {selected_city}")
+        self.subfields = self.csv_cleaned.loc[self.csv_cleaned['COMMUNITY AREA NAME'] == self.selectorcombo.get()]
+        print(self.subfields.head())
+        x_axis = 'Months'
+        y_axis='Energy Consumption'
+        def upleft(self):
+            # Figure = KWH Average by month
+            self.ax1.clear()
+            janind = self.subfields.columns.get_loc("KWH JANUARY 2010")
+            self.ax1.bar(range(1, 13),
+                         (self.subfields.iloc[:,  range(janind, (janind + 12))]).mean())
+            self.ax1.set_title('KWH Monthly Average')
+            self.ax1.set_xlabel(x_axis); self.ax1.set_ylabel(y_axis)
+            self.chart1.draw()
+        def upright(self):
+            # Figure = THERM value by month
+            self.ax2.clear()
+            janind = self.subfields.columns.get_loc("THERM JANUARY 2010")
+            self.ax2.bar(range(1, 13),
+                         (self.subfields.iloc[:, range(janind, (janind + 12))]).mean())
+            self.ax2.set_title('THERM Monthly Average')
+            self.ax2.set_xlabel(x_axis); self.ax2.set_ylabel(y_axis)
+            self.chart2.draw()
+        def botleft(self):
+            # Figure = Max KWH value by month 
+            self.ax3.clear()
+            janind = self.subfields.columns.get_loc("KWH JANUARY 2010")
+            self.ax3.bar(range(1, 13),
+                         (self.subfields.iloc[:,  range(janind, (janind + 12))]).max())
+            self.ax3.set_title('KWH Monthly Maximum')
+            self.ax3.set_xlabel(x_axis); self.ax1.set_ylabel(y_axis)
+            self.chart3.draw()
+        def botfig(self):
+            # Figure = Min THERM value by month
+            self.ax4.clear()
+            janind = self.subfields.columns.get_loc("THERM JANUARY 2010")
+            self.ax4.bar(range(1, 13),
+                         (self.subfields.iloc[:,  range(janind, (janind + 12))]).min())
+            self.ax4.set_title('THERM Monthly Minimum')
+            self.ax4.set_xlabel(x_axis);self.ax1.set_ylabel(y_axis)
+            self.chart4.draw()
+        upleft(self)
+        upright(self)
+        botleft(self)
+        botfig(self)
 
-        #Mothly KWH Average
-        OriginalDataSet[:,OriginalDataSet.columns.get_loc('KWH JANUARY 2010','KWH FEBRUARY 2010','KWH MARCH 2010','KWH APRIL 2010','KWH MAY 2010','KWH JUNE 2010','KWH JULY 2010','KWH AUGUST 2010','KWH SEPTEMBER 2010','KWH OCTOBER 2010','KWH NOVEMBER 2010','KWH DECEMBER 2010')].mean()
-        
-        #Monthly THERM Average
-        OriginalDataSet[:,OriginalDataSet.columns.get_loc('THERM JANUARY 2010','THERM FEBRUARY 2010','THERM MARCH 2010','TERM APRIL 2010','THERM MAY 2010','THERM JUNE 2010','THERM JULY 2010','THERM AUGUST 2010','THERM SEPTEMBER 2010','THERM OCTOBER 2010','THERM NOVEMBER 2010','THERM DECEMBER 2010')].mean()
-
-        #KWH Standard Deviation
-
-        #THERM Standard Deviation
-
-
-        #Creating the Plot Function Class
-class Plot:
-        def __init__(self, root,OriginalDataSet:"Plot",x:"Plot",y:"Plot",axs:"Plot",fig:"Plot"): 
-            fig=Figure(figsize=(3,3),dpi=100)
-            fig, axs = plt.subplots(2, 2,sharex=True)
-            fig.suptitle('Energy Usage Summary')
-            self.x=[i**2 for i in range (101)]
-            self.y=[i**2 for i in range (101)]
-            self.axs= axs
-            self.fig = fig
-
-            
-        def OriginalDataSet(self,root,fig):    
-            self.root= OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv')
-            self.OriginalDataSet=OriginalDataSet.dropna()
-            OriginalDataSet.head()
-            self.fig=fig
-            print(OriginalDataSet)
-
-        #Creating a Monthly KWH Average Class
-            class Monthly_KWH_Average('Energy_Usage_2010.csv'):
-                x=[i**2 for i in range (101)]
-                y=[i**2 for i in range (101)]
-                axs=(x,y)
-
-        #Monthly KWH Canvas
-                plt.hist(OriginalDataSet.Monthly_KWH_Average,bins=100,density=True,histtype='bar')
-                axs[0, 0].plot(x, y,color='#4257f5')
-                axs[0, 0].set_title('Monthly KWH Average')
-                Monthly_KWH_Average = fig.add_subplot(111)
-
-
-        #Establishing labels for x and y axes
-                for ax in axs.flat:
-                    ax.set(xlabel='Months', ylabel='Consumption')
-
-
-        #Making x axis invisible
-                for ax in axs.flat:
-                    ax.label_outer()
-plt.show() 
-
-Monthly_KWH_Average = FigureCanvasTkAgg(OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv'))
-Monthly_KWH_Average = tk.Canvas(OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv'))
-Monthly_KWH_Average.place(x=50, y=130, width=234, height=140)
-Monthly_KWH_Average.draw() #Or Canvas.draw()?
-
-        #Placing the Canvas onto the TKinter Window
-        
-Monthly_KWH_Average.get_tk_widget().pack()
-        
-        #Creating the MatplotLib Tool Bar
-        
-toolbar=NavigationToolbar2Tk(Monthly_KWH_Average,OriginalDataSet=pd.read_csv('Energy_Usage_2010.csv'))
-
-        #Placing the Tool Bar onto the Tkinter Window
-Monthly_KWH_Average.get_tk_widget().pack()
-
-        #Button for displaying the plot
-self _Energy_Usage_Summary_button=tk.Button(self=root, command = plot, height =2, width = 10,text ="Plot Summary")
-
-        #Placing the button onto the main window
-Energy_Usage_Summary_button.pack()
-        
-root.mainloop() #Running the GUI
-
-        #END OF TRIAL CODE
-
-
-
-        # these canvases are broken, fix them
-self.__Monthly_KWH_Average = tk.Canvas(root)
-self.__Monthly_KWH_Average.place(x=50, y=130, width=234, height=140)
-Monthly_KWH_Average.draw()
-
-    #    self.__GLineEdit_985 = tk.Canvas(root)
-    #    self.__GLineEdit_985.place(x=310, y=130, width=239, height=139)
-    #    self.__GLineEdit_392 = tk.Canvas(root)
-    #    self.__GLineEdit_392.place(x=50, y=290, width=233, height=157)
-
-    #    self.__GLineEdit_700 = tk.Canvas(root)
-    #    self.__GLineEdit_700.place(x=310, y=290, width=234, height=158)
-
-def __Load_CSV_command(self):
-            filePath = fd.askopenfilename(initialdir='.')
-            try:
-                self.__df = pd.read_csv(filePath)
-                self.__df = self.__df.dropna()
-                self.__Cities_List['values'] = list(self.__df['COMMUNITY AREA NAME'].unique())
-            except:
-            # quick and dirty, desired behavior would be to show a notification pop up that says
-            # "nope!"
-                print('nope')
-
-    # desired behavior: select one area, show 4 plots drawn on 4 canvases of that area: 
-    # top left: bar chart, average KWH by month
-    # top right: bar chart, average THERM by month
-    # bottom left and bottom right up to you
-            def __comboBoxCb(self, event=None):
-                self.__subdf = self.__df.loc[self.__df['COMMUNITY AREA NAME'] == self.__Cities_List.get()]
-            print(self.__subdf.head())
-            fig1 = Figure(figsize=(self.__GLineEdit_392.winfo_width, self.__GLineEdit_392.winfo_height), dpi=100)
-            ax1 = fig1.add_subplot(111)
-            self.__subdf.iloc[:, range(self.__subdf.columns.get_loc['KWH JANUARY 2010'], 12)].mean().plot.bar(ax=ax1)
-
-
-if __name__ == "__main__":
+def main():
     root = tk.Tk()
     app = App(root)
     root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
